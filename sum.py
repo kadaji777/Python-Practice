b33c54d91a29db45b4d5033ac15d0aff2d842bd8
rocliff kadaji <roinwani@gmail.com>
2025-04-25 16:56:04 +0300

    Sum of Digits of a Number   do the functions saparately

---
+++ sum.py
@@ -0,0 +1,10 @@
+def sum_of_digits(n):
+    total = 0
+    while n > 0:
+        digit = n % 10
+        total += digit
+        n = n // 10
+    return total
+
+# Example
+print(sum_of_digits(1234))  # Output is 10
