304e3e94c608bc211d5a60fdbbaf2d77bcea11ce
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-04-26 14:32:22 +0300

    explanation of my code

--- elements.py
+++ elements.py
@@ -6,3 +6,8 @@ def sum_list_elements(numbers):
 
 # Example
 print(sum_list_elements([1, 2, 3, 4, 5]))  # Output: 15
+#explanation
+#You loop through each item in the list.
+
+# Then you add each item to a total variable.
+#The return is the final total.
