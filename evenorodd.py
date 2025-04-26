7af003f9054210451b232ad6c2ecf2b070e6cc02
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-04-26 19:42:15 +0300

    code logic

--- evenorodd.py
+++ evenorodd.py
@@ -6,3 +6,9 @@ def check_even_odd(number):
 
 # Example
 print(check_even_odd(7))   #the output will be odd
+ #Explanation
+#A number is even if it’s divisible by 2 (i.e., remainder is 0).
+
+#Use the modulus operator % to check that.
+
+#Return the appropriate string.
